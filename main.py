a777ff6dd2c6ad895dfc73f417bba73dcad99f24
Michael Griffits <griffitsm@stu.otc.school.nz>
2022-08-17 14:13:58 +1200

    Second iteration of Main Menu

--- main.py
+++ main.py
@@ -8,33 +8,68 @@ main_menu_frame = Frame(main_menu)
 main_menu_frame.grid(row=0, column=0)
 character_creation_frame = Frame(main_menu)
 character_creation_frame.grid_forget()
+information_library_frame = Frame(main_menu)
+information_library_frame.grid_forget()
+load_character_frame = Frame(main_menu)
+load_character_frame.grid_forget()
 
 
-# Main Menu Functions
+# Quit Program Function
+def quit_program():
+    quit()
+
+
+# Main Menu To Information Library Function
+def open_information_library():
+    main_menu_frame.grid_forget()
+    information_library_frame.grid(row=0, column=0)
+
+
+# Main Menu To Character Creation Function
 def open_character_creation():
     main_menu_frame.grid_forget()
     character_creation_frame.grid(row=0, column=0)
 
 
-# Back To Main Menu function
+# Main Menu To Load Character Menu function
+def open_load_character():
+    main_menu_frame.grid_forget()
+    information_library_frame.grid(row=0, column=0)
+
+
+# Back To Main Menu From Character Creation function
 def back_to_main_menu():
     character_creation_frame.grid_forget()
+    information_library_frame.grid_forget()
+    load_character_frame.grid_forget()
     main_menu_frame.grid(row=0, column=0)
 
 
 # Main Menu Widgets Creation
 menu_name = Label(main_menu_frame, text="Dungeons & Dragons", font="helvetica 40 bold")
 menu_sub_name = Label(main_menu_frame, text="Character Creator", font="helvetica 15 bold")
-create_character_button = Button(main_menu_frame, text="CREATE A CHARACTER", font="helvetica 20 bold", command=lambda: open_character_creation())
-information_library_button = Button(main_menu_frame, text="INFORMATION LIBRARY", font="helvetica 20 bold")
-load_character_button = Button(main_menu_frame, text="LOAD CHARACTER", font="helvetica 20 bold")
-quit_button = Button(main_menu_frame, text="QUIT", font="helvetica 20 bold")
+create_character_button = Button(main_menu_frame, text="CREATE A CHARACTER", font="helvetica 20 bold",
+                                 command=lambda: open_character_creation())
+information_library_button = Button(main_menu_frame, text="INFORMATION LIBRARY", font="helvetica 20 bold",
+                                    command=lambda: open_information_library())
+load_character_button = Button(main_menu_frame, text="LOAD CHARACTER", font="helvetica 20 bold",
+                               command=lambda : open_load_character())
+quit_button = Button(main_menu_frame, text="QUIT", font="helvetica 20 bold",
+                     command=lambda: quit_program())
+
+# Information Library Menu Widgets Creation
+back_button_il = Button(information_library_frame, text="BACK", font="helvetica 20 bold",
+                        command=lambda: back_to_main_menu())
+
+# Information Library Menu Widgets Placement
+back_button_il.grid(row=0, column=0)
 
 # Character Creation Menu Widgets Creation
-back_button = Button(character_creation_frame, text="BACK", font="helvetica 20 bold", command=lambda: back_to_main_menu())
+back_button_cc = Button(character_creation_frame, text="BACK", font="helvetica 20 bold",
+                        command=lambda: back_to_main_menu())
 
 # Character Creation Menu Widgets Placement
-back_button.grid(row=0, column=0)
+back_button_cc.grid(row=0, column=0)
 
 # Main Menu Widgets Placement
 menu_name.grid(row=0, column=2, padx=125, pady=15)
